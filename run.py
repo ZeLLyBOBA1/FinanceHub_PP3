8c5fcebdfa31e314cf34303d8d7d56629da6c21b
Dmitry Loshch <loschdmitry2005@gmail.com>
2024-07-28 15:03:27 +0000

    Added new functions

--- run.py
+++ run.py
@@ -25,8 +25,13 @@ data = userdata.get_all_values()
 
 sleep = time.sleep
 
-username = "username"
-pincode = 1111
+username_cell = None
+pincode_cell = None
+balance_cell = None
+
+username = None
+pincode = None
+balance = None
 
 #clears console
 def console_clear():
@@ -41,6 +46,13 @@ def print_logo():
 
 #loads login page
 def load_login_page():
+    global username
+    global pincode
+    global balance
+
+    global username_cell
+    global pincode_cell
+    global balance_cell
 
     console_clear()
     print_logo()
@@ -61,13 +73,18 @@ def load_login_page():
         pincode = input("Enter your pin: ")
       
         if(userdata.find(username) != None and pincode == userdata.cell(userdata.find(username).row, userdata.find(username).col + 1).value):
+            username_cell = userdata.cell(userdata.find(username).row, userdata.find(username).col)
+            pincode_cell = userdata.cell(userdata.find(username).row, userdata.find(username).col + 1)
+            balance_cell = userdata.cell(userdata.find(username).row, userdata.find(username).col + 2)
+            balance = balance_cell.value
 
             print("\n-- YOU ARE LOGED IN --")
             sleep(2)
+            load_main_page()
 
         else:
 
-            print("Wrong username or pin")
+            print("-- WRONG USERNAME OR PIN --")
             sleep(2) 
             load_login_page()
 
@@ -83,6 +100,8 @@ def load_login_page():
 
 #loads signup page
 def load_signup_page():
+    global username
+    global pincode
     
     console_clear()
     print_logo()
@@ -106,10 +125,14 @@ def load_signup_page():
             if(userdata.find(username) == None):
                 # userdata.append_row([username])
                 pincode = random.randint(1000,9999)
+                balance = 0
                 print(f"\nYour pin is ==> {pincode}")
-                userdata.append_row([username, pincode])
+                userdata.append_row([username, pincode , balance])
                 print("\nMake sure to write it down and press 'Enter'")
                 input()
+
+                sleep(2)
+                load_main_page()
             else:
                 print("This username is already in use")
                 load_signup_page()
@@ -148,6 +171,105 @@ def load_entry_page():
         sleep(2)
         load_entry_page()
 
+def load_main_page():
+    global username
+    global pincode
+    global balance
+
+    global username_cell
+    global pincode_cell
+    global balance_cell
+
+    console_clear()
+    print_logo()
+
+    print(f"Welcome, {username}!")
+    print(f"\n=============================== \nYour balance is {balance}$ \n=============================== \n")
+    print("Choose an option below:\n")
+    print("(1) Deposit money    (3) Withdraw money")
+    print("(2) Change my pin    (4) Delete my account")
+    print("            (0) Log out")
+
+    choosenoption = input("\n>> ")
+
+    if(choosenoption == "0"):
+        username = None
+        pincode = None
+        load_entry_page()
+
+    elif(choosenoption == "1"):
+        load_deposit_page()
+
+    elif(choosenoption == "2"):
+        load_pin_change_page()
+
+    elif(choosenoption == "3"):
+        load_withdrawal_page()
+
+    elif(choosenoption == "4"):
+        load_delete_page()
+    
+    elif(choosenoption == "404"):
+        print(f"username_cell = {username_cell}")
+        print(f"pincode_cell = {pincode_cell}")
+        print(f"balance_cell = {balance_cell}")
+        print(f"username = {username}")
+        print(f"pincode = {pincode}")
+        print(f"balance = {balance}")
+
+    else:
+        print("\n-- OPTION OUT OF RANGE --")
+        sleep(2)
+        load_main_page()
+
+def load_deposit_page():
+    global balance
+    
+    console_clear()
+    print_logo()
+
+    print("\nHow much would you like to deposit?")
+    deposit_amount = input("\n>> ")
+
+    console_clear()
+    print_logo()
+    print("\n -- PROCESSING OPERATION -- ")
+    sleep(2)
+
+    if(balance + abs(int(deposit_amount)) <= 100000000000):
+        print("\n -- SUCCESS -- ")
+        sleep(2)
+        load_main_page()
+
+    else:
+        print("\n -- You cant keep more than 100 billions on your balance --")
+        print("\nChoose your option below:")
+        print("\n(1) to try again \n\n - or - \n\n(0) to visit main page")
+        choosenoption = input("\n>> ")
+        
+        if(choosenoption == "1"):
+            load_deposit_page()
+        
+        elif(choosenoption == "0"):
+            load_main_page()
+
+        else:
+            print("\n-- OPTION OUT OF RANGE --")
+
+def load_withdrawal_page():
+
+    console_clear()
+    print_logo()
+
+def load_pin_change_page():
+
+    console_clear()
+    print_logo()
+
+def load_delete_page():
+
+    console_clear()
+    print_logo()
 
 # SHEET.worksheet('userdata').append_row(['demon228', '6366'])
 # found = SHEET.worksheet('userdata').find('huesosik')
